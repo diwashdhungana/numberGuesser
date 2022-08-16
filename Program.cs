493b9572c871eab51efca7ff251b7bd49beb481a
374108 <109555081+374108@users.noreply.github.com>
2022-08-16 13:26:14 -0500

    number Guesser project push

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Number
+    {
+        static void Main(string[] args)
+        {
+            Random randomNum = new Random();
+            int winningNum = randomNum.Next(0,100);
+            bool win = false;
+
+            do 
+            {
+                Console.WriteLine("Guess a number between 0 and 100");
+                var choosenNum = Console.ReadLine();
+
+                int i = int.Parse(choosenNum);
+
+                if (i > winningNum)
+                {
+                    Console.WriteLine("High!!! choose low number ");
+                }
+
+                else if (i < winningNum)
+                {
+                    Console.WriteLine("Low!!! choose higher number");
+                }
+
+                else if (i == winningNum)
+                {
+                    Console.WriteLine("you choose the right number");
+                    Console.ReadLine();
+                    win = true ;
+
+                }
+
+            }while (win == false);
+        }
+    }
+}
